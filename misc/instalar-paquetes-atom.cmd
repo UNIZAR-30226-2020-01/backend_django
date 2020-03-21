710e3b8417f8aaa72d04e60e013c76b6a21729b4
nestor <nestormg98@gmail.com>
2020-03-21 13:29:49 +0100

    Mis paquetes de atom

---
+++ misc/instalar-paquetes-atom.cmd
@@ -0,0 +1,7 @@
+:: si este script no va copiais el comando y ya 
+:: obviamente teneis que tener atom antes
+
+:: el fichero atom-requirements lo he generado con 
+:: apm list --installed --bare > atom-requirements.txt
+:: fuente: https://coderwall.com/p/gbrpqq/share-export-inport-your-atom-installed-packages-list
+apm install --packages-file atom-requirements.txt
