54be496e32622f93c29efd58dcbd618b3b760583
jtambo99 <jtambo@live.com>
2020-03-22 19:50:21 +0100

    Usuario deriva del modelo User de DJango

--- servidor/musica/models.py
+++ servidor/musica/models.py
@@ -6,7 +6,7 @@
 #   * Remove `managed = True` lines if you wish to allow Django to create, modify, and delete the table
 # Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
-
+from django.contrib.auth.models import User
 from utils.lyrics.lyrics import Lyrics_api
 
 # para señales (ahora no las usamos):
@@ -117,11 +117,7 @@ class Audio(models.Model):
 
 ##### TODO: cambiar esta clase para usar la predefinida de django. Extenderla con herencia o sobreescribir sus campos,
 ##### no se que es mejor. Nos facilitara implementar la autentificacion y demas, creo.
-class Usuario(models.Model):
-    #id = models.IntegerField(primary_key=True)
-    name = models.CharField(max_length=50, unique=True)
-    correo = models.CharField(max_length=50)
-    contraseña = models.CharField(max_length=100)
+class Usuario(User):
     podcasts = models.ManyToManyField(Podcast)
     siguiendo = models.ManyToManyField('self', symmetrical=False, related_name='seguidor')
     #seguido = models.ManyToManyField('self', symmetrical=False, related_name='seguido')
