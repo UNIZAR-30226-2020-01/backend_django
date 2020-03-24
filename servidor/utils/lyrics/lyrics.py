900bcbc29fa27803d7ac61f06c8d042800688c93
nestor <nestormg98@gmail.com>
2020-03-24 19:03:18 +0100

    Habia un fichero de mas

--- servidor/utils/lyrics/lyrics.py
+++ servidor/utils/lyrics/lyrics.py
@@ -1,6 +1,7 @@
 import requests
 
 # Wrapper de la api de lyricsovh:
+# TODO: Probar otras APIs para buscar en ellas cuando falle la primera:
 class Lyrics_api:
     # deberiamos citar a https://lyricsovh.docs.apiary.io en alguna parte
     def __init__(self, api="https://api.lyrics.ovh/v1/", lyrics_key='lyrics'):
@@ -11,8 +12,9 @@ class Lyrics_api:
         uri = self.api + artist + '/' + title # uri de la cancion, tipo https://api.lyrics.ovh/v1/ska-p/verguenza
         # print(uri)
         response = requests.get(uri)
-        if response.status_code != 200: # se podria poner una excepcion aqui
-            return 'These lyrics are not available (error: ' + str(response.status_code) + ')'
+        if response.status_code != 200: # arreglar esto que esta feo
+            # TODO: aquí se llamarí­a a otra API
+            return '' # de momento mejor la cadena vacia que lo de antes: These lyrics are not available (error: ' + str(response.status_code) + ')
         #print(response.text)
         return response.json()[self.lyrics_key] # devuelve la letra (el valor de la clave dada en el diccionario json)
 
