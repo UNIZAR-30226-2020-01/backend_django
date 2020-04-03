fa40f49898b44ce2461c75b76db472b5a762d44c
Pedro Tamargo <pedro.tamargo.allue@gmail.com>
2020-04-03 17:05:40 +0200

    Script de travis para CD

---
+++ docker/script_travis.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Script que ejecutara travis para hacer el deploy si el build pasa.
+
+USER='PS'
+IP='s7-rest.francecentral.cloudapp.azure.com'
+
+FILE=$(mktemp tmpXXX)
+
+echo $TRAVIS_RSA > $FILE
+
+ssh -i $FILE $USER@$IP << EOF
+  cd ~ps/backend_django
+  git pull origin master
+  # Paramos el servicio
+  sudo docker-compose down
+  # Reconstruimos la imagen
+  sudo docker-compose build
+  # Levantamos el servicio
+  sudo docker-compose up -d
+EOF
+
+rm $FILE
