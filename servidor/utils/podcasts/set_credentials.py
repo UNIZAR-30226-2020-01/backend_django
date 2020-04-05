3654cdfb307318eacebe5fc9310bf917217805ee
jtambo99 <jtambo@live.com>
2020-04-05 20:19:33 +0200

    Funciones básicas de podcasts

---
+++ servidor/utils/podcasts/set_credentials.py
@@ -0,0 +1,10 @@
+import os
+
+def the_secret_function():
+    os.environ['SECRET_KEY'] = 'algakbjdfsAWGEPIWN093562aversiahoranosequejatravisdecaracteresespeciales' # la de django
+    os.environ['SPOTIPY_CLIENT_ID'] = '6fddeaa1b496433dab3900cec8d6b45d' # spotify
+    os.environ['SPOTIPY_CLIENT_SECRET'] = '7033176f1e7240b188ae2a44684af4ef'
+    os.environ['PASSWORD_POSTGRESQL'] = 'lQ9n_mpqPGxX5TumMLOWA62notw2MmJB' # postgres
+    os.environ['SOCIAL_AUTH_GOOGLE_OAUTH2_KEY'] = '968247949241-bu1gmcsukgvth00mq7haehpb57aujl17.apps.googleusercontent.com' # google
+    os.environ['SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET'] = 'XJuOonDd1AhsgLC8loUyQ0-_'
+    os.environ['LISTENNOTES_KEY'] = '9c23efd79d5f47d9b05e1f5125196d65'
